4b3338f9695ee016b2c1f223da4c0580cf122e90
Victor Lupascu <sinajeen@gmail.com>
2021-06-07 00:29:34 -0500

    rearange css and html some, add few more styles for better visualisation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // renders current date and time in the jumbotron
 setInterval(function () {
-    var x = moment().format('MM/DD/YYYY hh:mm:ss');
-    $('#currentDay').text(x);
+    var x = moment().format('dddd, MMM/DD/YYYY, hh:mm:ss a ');
+    $('#current-day').text(x);
 }, 1000);
-
 
